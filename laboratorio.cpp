0392ab17b831ef635f45ad2c2b44a458c014ce93
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-10-14 13:45:30 -0500

    se modifico la funcion mostrar() y se añadio un cout << p al iterador

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -19,13 +19,21 @@ void Laboratorio::agregarPersonaje(const Computadora &p)
 
 void Laboratorio::mostrar()
 {
+    cout << left;
+    cout << setw(20) << "Sistema Operativo";
+    cout << setw(20) << "Nombre Usuario";
+    cout << setw(20) << "Almacenamiento";
+    cout << setw(15) << "RAM";
+    cout << endl;
+
     for (size_t i = 0; i < cont; i++){
         Computadora &p = arreglo[i];
-        cout<<"Sistema Operativo: "<<p.getSo()<<endl;
-        cout<<"Nombre de Usuario: "<<p.getNomuser()<<endl;
-        cout<<"Almacenamiento: "<<p.getAlmacenamiento()<<endl;
-        cout<<"RAM: "<<p.getRam()<<endl;
-        cout<<endl;
+        cout << p;
+        // cout<<"Sistema Operativo: "<<p.getSo()<<endl;
+        // cout<<"Nombre de Usuario: "<<p.getNomuser()<<endl;
+        // cout<<"Almacenamiento: "<<p.getAlmacenamiento()<<endl;
+        // cout<<"RAM: "<<p.getRam()<<endl;
+        // cout<<endl;
     }
     
 }
