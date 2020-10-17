6e7b7bb30ba400e8f6b1894f3aa8a47e25a0d2fc
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-10-17 17:10:45 -0500

    se comento todo el codigo dentro del main y se dejaron las 3 funciones de recuperar() 2 veces y 1 de mostrar()

--- main.cpp
+++ main.cpp
@@ -7,29 +7,40 @@
 using namespace std;
 
 int main(){
-    Computadora comp01;
-    comp01.setSo("Windows");
-    comp01.setNomuser("Eduardo");
-    comp01.setAlmacenamiento("500GB");
-    comp01.setRam(8);
-
-    Computadora comp02;
-    comp02.setSo("MacOS");
-    comp02.setNomuser("Luis");
-    comp02.setAlmacenamiento("1TB");
-    comp02.setRam(16);
-
 
     Laboratorio lb;
 
-    lb.agregarPersonaje(comp01);
-    lb.agregarPersonaje(comp02);
-
-    Computadora comp03;
-    cin >> comp03;
-    cout << endl;
-    lb << comp03;
-
+    lb.recuperar();
+    lb.recuperar();
     lb.mostrar();
+
+    // Computadora comp01;
+    // comp01.setSo("Windows");
+    // comp01.setNomuser("Eduardo");
+    // comp01.setAlmacenamiento("500GB");
+    // comp01.setRam(8);
+
+    // Computadora comp02;
+    // comp02.setSo("MacOS");
+    // comp02.setNomuser("Luis");
+    // comp02.setAlmacenamiento("1TB");
+    // comp02.setRam(16);
+
+
+    // Laboratorio lb;
+
+    // lb.agregarPersonaje(comp01);
+    // lb.agregarPersonaje(comp02);
+
+    // Computadora comp03;
+    // cin >> comp03;
+    // cout << endl;
+    // lb << comp03;
+
+    // lb.mostrar();
+    // lb.respaldar_tabla();
+    // lb.respaldar();
+
+
     return 0;
 } 
